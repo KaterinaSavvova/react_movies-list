1303e267cdeaa0528854efb929c7e457ec3a3034
KatrinaSavvova <kat.savvek@gmail.com>
2020-08-31 21:28:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.78b7be427c0e464d62ef843b49632011.js"
 );
 
 self.addEventListener('message', (event) => {
